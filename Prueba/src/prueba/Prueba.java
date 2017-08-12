44bfffbc0abbf89a9ea17fc606384a66c159b504
Lorena Rodriguez <Lorena Rodriguez@Lorena>
2017-08-12 17:17:42 -0500

    Taller clonación del ejercicio de prueba de un compañero

--- Prueba/src/prueba/Prueba.java
+++ Prueba/src/prueba/Prueba.java
@@ -6,7 +6,7 @@ public class Prueba {
 
     public static void main(String[] args) {
     for (int i=0; i<100; i++)
-        System.out.println("Hola mundo");
+        System.out.println("Este mensaje ha sido modificado, taller 1 clonación");
     
     }
     
